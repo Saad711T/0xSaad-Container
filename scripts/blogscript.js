13d7246be3a3bb8f71a6d26fe3531a32cb374b72
0xSaad <isaadmalkyxi@gmail.com>
2025-07-13 22:11:41 +0300

    Edit pages

---
+++ scripts/blogscript.js
@@ -0,0 +1,5 @@
+    fetch("https://raw.githubusercontent.com/Saad711T/0xSaad-Container/blog/README.md")
+      .then(res => res.text())
+      .then(text => {
+        document.getElementById("readme").innerHTML = marked.parse(text);
+      });
