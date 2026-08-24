6cfd8c28678872d6451ee06552b676d72f999eef
0xSaad <75455552+Saad711T@users.noreply.github.com>
2026-08-25 00:09:46 +0300

    Remove bookscript.js

--- softwares/Matrices-minibook/bookscript.js
+++
@@ -1,5 +0,0 @@
-    fetch("https://raw.githubusercontent.com/Saad711T/0xSaad-Container/refs/heads/main/softwares/Matrices-minibook/README.md")
-      .then(res => res.text())
-      .then(text => {
-        document.getElementById("readme").innerHTML = marked.parse(text);
-      });
